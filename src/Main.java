34576af6fdd9ba875764d994e2db2da2cfb87fd7
teeredtech <tolulope.oredein@redtechlimied.com>
2023-10-01 18:31:46 +0100

    Thread 2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(Thread.currentThread());
-        System.out.println("Inside main");
-
         Thread t1 = new Thread(new SmsCampaign());
         Thread t2 = new Thread(new EmailCampaign());
         t1.setName("SmsCampaign");
         t2.setName("EmailCampaign");
-        t1.setPriority(Thread.MIN_PRIORITY);
+        t1.setPriority(Thread.MAX_PRIORITY);
         t2.setPriority(Thread.MAX_PRIORITY);
         t1.start();
         t2.start();
+
+        try {
+            t2.join();
+        }
+        catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Inside main");
     }
 
     public static class SmsCampaign implements Runnable{
@@ -20,7 +26,7 @@ public class Main {
             for (int i =1; i<10; i++) {
                 System.out.println(Thread.currentThread().getName());
                 if(i==5){
-                    Thread.currentThread().yield();
+                    Thread.currentThread().yield(); // Thread ready to release CPU Usage
                 }
             }
 
